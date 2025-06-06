95a4ef16ba39aa4a3853492e494e37419e226f2d
OliwierMaciejewski <oliwier.maciejewskix@gmail.com>
2025-06-06 21:40:36 +0200

    Temat II

---
+++ Zadanie 2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2._1
+{
+    internal class Program
+    {
+        /*
+        Napisać program wczytujący liczbę całkowitą i wypisujący na ekranie informację czy jest to 
+        liczba parzysta, czy też nieparzysta.
+        */
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Podaj liczbecalkowita: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+                Console.WriteLine("liczba {0} jest parzysta.", a);
+            else
+                Console.WriteLine("liczba {0} jest nieparzysta.", a);
+            Console.ReadKey(true); //pauza
+        }
+    }
+}
