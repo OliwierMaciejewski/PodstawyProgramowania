d41bc96aab5cef9663cb0f84b68f9ffe0af4344b
OliwierMaciejewski <oliwier.maciejewskix@gmail.com>
2025-06-08 18:31:55 +0200

    Temat VIII

---
+++ Zadanie 8.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_8._1
+{
+    internal class Program
+    {
+        /*
+        Napisz funkcję , która oblicza wartość silni z zadanej przez argument wywołania funkcji 
+        wartości liczby naturalnej. Utwórz wersję rekurencyjną tej funkcji. 
+        Napisz program testowy wykorzystujący napisaną funkcję.
+        */
+        
+        // Definicja funkcji rekurencyjnej
+        static long Silnia(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * Silnia(n - 1);
+        }
+
+        // Przykładowe użycie funkcji rekurencyjnej
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n < 0)
+                Console.WriteLine("Błędny argument!");
+            else
+                Console.WriteLine("{0}! = {1}", n, Silnia(n));
+
+            Console.ReadKey(true); //pauza
+        }
+    }
+}
---
+++ Zadanie 8.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_8._2
+{
+    internal class Program
+    {
+        /*
+        Napisz funkcję, która wyznacza n-ty wyraz ciągu Fibonacciego zadany przez argument 
+        wywołania funkcji wartości liczby naturalnej. Utwórz wersję rekurencyjną tej funkcji. 
+        Napisz program testowy wykorzystujący napisaną funkcję.
+        */
+       
+        //definicja funkcji rekurencyjnej
+        static long Fib(long n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+
+        //przykładowe użycie funkcji rekurencyjnej
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj n: ");
+            long n = Convert.ToInt64(Console.ReadLine());
+
+            if (n < 0)
+                Console.WriteLine("Błędny parametr!");
+            else
+                Console.WriteLine($"Fib({n}) = {Fib(n)}");
+
+            Console.ReadKey(true);
+        }
+    }
+}
